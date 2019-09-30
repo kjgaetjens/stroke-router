bec4162d78427e1078591737699d9eeeab40335e
Eric Snover <ersnover@gmail.com>
2019-09-30 09:55:27 -0500

    initial commit, file structure and basic redux store init

---
+++ ems-client/src/store/reducer.js
@@ -0,0 +1,12 @@
+const initialState = {
+    isAuth: true,
+    userId: 1,
+}
+
+const reducer = (state = initialState, action) {
+    switch(action.type) {
+        default: return state
+    }
+}
+
+export default reducer
