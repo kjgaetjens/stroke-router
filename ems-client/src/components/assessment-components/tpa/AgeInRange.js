e28310521beae29fc9a64cb625bf463e2d4a132e
Kelsey Gaetjens <kjgaetjens@gmail.com>
2019-10-04 14:21:57 -0500

    initial styling for ems assessment

--- ems-client/src/components/assessment-components/tpa/AgeInRange.js
+++ ems-client/src/components/assessment-components/tpa/AgeInRange.js
@@ -8,12 +8,25 @@ function AgeInRange(props) {
     }
 
     return (
-        <div>
-            AgeInRange
-            <button onClick={() => submitAnswer(true)}>Yes</button>
-            <button onClick={() => submitAnswer(false)}>No</button>
-            <button onClick={() => props.nextQuestion('LastKnownWell')}>Unknown</button>
+        <React.Fragment>
+        <div className="assessment-header">
+            <span>{'< Back'}</span>
+            <h3>tPA Exclusion Assessment</h3>
         </div>
+        <div className="progress-border">
+            <div className="progress-fill"></div>
+        </div>
+        <div className="assessment-container">
+            <div className="question-container">
+                <h1>Is the patient over the age of 18 and under the age of 80?</h1>
+            </div>
+            <div className="answer-container">
+                <button onClick={() => submitAnswer(true)}>Yes</button>
+                <button onClick={() => submitAnswer(false)}>No</button>
+                <button onClick={() => props.nextQuestion('LastKnownWell')}>Unknown</button>
+            </div>
+        </div>
+        </React.Fragment>
     );
 
 }
